23cad3e2f5c38b4752f85aec4ec1bd45c67bef1d
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-15 18:07:52 +0530

    uc3moodanalyser

--- com/mood/MoodAnalyzer.java
+++ com/mood/MoodAnalyzer.java
@@ -3,14 +3,14 @@ package com.mood;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class MoodAnalyzerTest {
     @Test
     public void testMoodAnalysis() throws Exception {
 
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        moodAnalyzer.message=null;
-        String mood = moodAnalyzer.analyseMood();
+        String mood = moodAnalyzer.analyseMood("I am in Any Mood");
         Assertions.assertEquals("HAPPY",mood);
     }
+
+
 }
