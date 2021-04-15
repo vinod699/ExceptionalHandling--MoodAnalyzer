30bb3126a3ed97f4f0a0ebf9c0015d4b484fc079
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-15 18:13:53 +0530

    uc3.1moodanalyser

--- com/mood/MoodAnalyzer.java
+++ com/mood/MoodAnalyzer.java
@@ -2,15 +2,29 @@ package com.mood;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
 
 public class MoodAnalyzerTest {
     @Test
-    public void testMoodAnalysis() throws Exception {
-
+    public void testMoodAnalysis() throws MoodAnalysisException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.analyseMood("I am in Any Mood");
+        String mood = moodAnalyzer.analyseMood("I am in sad mood");
+        Assertions.assertEquals("SAD",mood);
+    }
+    @Test
+    public void whenHappyReturnHappy()throws MoodAnalysisException{
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyseMood("I am in happy mood");
         Assertions.assertEquals("HAPPY",mood);
     }
-
+    @SuppressWarnings("depricated")
+    @Test
+    public void whenNullthrowException(){
+        ExpectedException expectedException = ExpectedException.none();
+        expectedException.expect(MoodAnalysisException.class);
+        String mood="NULL";
+        Assertions.assertEquals("NULL",mood);
+    }
 
 }
